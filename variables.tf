506dcdac9b7849b520295e79631f5e59616f3a52
simpsonhomerjay <124053675+simpsonhomerjay@users.noreply.github.com>
2023-02-02 07:59:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
+variable "region" {
+  default = "us-east-2"
+}
+
 variable "ami" {
   default = "ami-0471021a4ee6bfcce"
 }
 
 variable "insttype" {
   default = "t2.small"
-}
-
-variable "region" {
-  default = "us-east-2"
 }
 
 variable "sgid" {
